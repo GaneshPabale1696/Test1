7605656b9090b5bc61477a955e91e1dd474b51f9
Ganesh Pabale <134202909+GaneshPabale1696@users.noreply.github.com>
2023-06-06 23:49:10 +0530

    This my first github commit

--- src/test/java/DemoTest/Test1/DropDown.java
+++ src/test/java/DemoTest/Test1/DropDown.java
@@ -1,25 +1,37 @@
 package DemoTest.Test1;
 
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DropDown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		//launch chrome browser
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\prach\\Desktop\\DemoTestMavenProject\\Test1\\Drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\prach\\Desktop\\DemoTestMavenProject\\Test1\\Drivers\\chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops); // launch chrome
 
 		//open https://www.opencart.com/index.php?route=account/register
 		driver.get("https://www.opencart.com/index.php?route=account/register");
+		
+		Thread.sleep(50000);
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		WebElement element = driver.findElement(By.id("input-country"));
 
